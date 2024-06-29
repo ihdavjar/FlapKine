8c7accdac2d3937f48b9ca68b61a58b8c5a7808b
ihdavjar <raj.31@iitj.ac.in>
2024-06-29 15:40:14 +0530

    Added flexibility (functional and transfrom classes).

---
+++ src/core/transforms/flexibility.py
@@ -0,0 +1,40 @@
+import numpy as np
+from functional_flexibility import functional_Flexibility_type1 
+
+class Flexibility_Transform:
+    def __call__(self, input, t):
+        raise NotImplementedError("Each transform must implement the __call__ method.")
+
+class Constant(Flexibility_Transform):
+    def __call__(self, input, t):
+        return input
+    
+class Flexibility_type1(Flexibility_Transform):
+    def __init__(self, x, y, z, major_axis, minor_axis):
+        # x: bool
+        # y: bool
+        # z: bool
+        # major_axis: bool
+        # minor_axis: bool
+
+        self.x = x
+        self.y = y
+        self.z = z
+        self.major_axis = major_axis
+        self.minor_axis = minor_axis
+    
+    def __call__(self, input, t):
+        # input: np.array of shape (n, 3) i.e (x, y, z)
+        # output: np.array of shape (n, 3) i.e (x, y, z) at time t
+        if self.x:
+            input[:, 0] = input[:, 0] + np.array([functional_Flexibility_type1(y, z, t, self.major_axis, self.minor_axis) for y, z in input[:, 1:]])
+
+        if self.y:
+            input[:, 1] = input[:, 1] + np.array([functional_Flexibility_type1(x, z, t, self.major_axis, self.minor_axis) for x, z in input[:, [0,2]]])
+
+        if self.z:
+            input[:, 2] = input[:, 2] + np.array([functional_Flexibility_type1(x, y, t, self.major_axis, self.minor_axis) for x, y in input[:, [0,1]]])
+
+        
+        return input
+    
---
+++ src/core/transforms/functional_flexibility.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+def functional_Flexibility_type1(x, y, t, major_axis, minor_axis):
+    C_R = 2*minor_axis
+    R = 2*major_axis
+
+    t = t/100
+    
+    C_r = C_R*((1-((x-major_axis)/major_axis)**2)**(0.5)) # Local chord length
+
+    Z_M_Root = 0.125*C_r
+
+    Z_M_x = (Z_M_Root/C_R)*(1-x/R)*(C_r)
+
+    Z_M_x_t = Z_M_x/np.tanh(2.9)*np.tanh(2.9*np.sin(2*np.pi*t + 0.4))   
+
+    p = 0.5
+
+    if (C_r != 0):
+        y_0 = (minor_axis-y)/C_r
+    
+    else: # At wingroot where C_r = 0
+        y_0 = 0
+
+    if (y_0<p):
+        Z_M_x_y_t = (Z_M_x_t/(p**2))*(2*p*y_0 - y_0**2)
+    
+    else:
+        Z_M_x_y_t = (Z_M_x_t/((1-p)**2))*(1-2*p+2*p*y_0-y_0**2)
+
+    return Z_M_x_y_t
